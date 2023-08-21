d2d9010277236d78c421a81a4561bdfeacffa7f0
KarlaBlancoVasquez <karlabco123@gmail.com>
2023-08-21 09:23:34 -0600

    actualizar en codigos

--- Codigos/01_Prueba_t_una_muestraR.R
+++ Codigos/01_Prueba_t_una_muestraR.R
@@ -2,3 +2,31 @@
 # 21/08/2023
 # Matrícula: 2133639
 
+
+# Importar datos ----------------------------------------------------------
+# Función read.csv (sirve para importar datos csv a R)
+
+mediciones <- read.csv("Codigos/mediciones.csv", header = TRUE)
+head(mediciones) # función head (sirve para ver primeros 6 datos)
+
+
+# Descriptivas ------------------------------------------------------------
+# medidas de tendencia central media. mediana, rango
+
+mean(mediciones$Altura)
+median(mediciones$Altura)
+range(mediciones$Altura)
+fivenum(mediciones$Altura)
+
+# medidaS de dispersión desviación estándar, varianza
+
+sd(mediciones$Altura)
+var(mediciones$Altura)
+
+
+# Gráficas ----------------------------------------------------------------
+
+boxplot(mediciones$Altura, col = "purple",
+        ylab = "Altura (cm)",
+        main = "Sitio 1",
+        ylim =c (6,14))
